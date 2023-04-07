f1eb51c0d30b446111e98ac45c8657824d515657
jeannot diambu <diambujeannot@gmail.com>
2023-04-07 11:43:49 +0100

    add file entity

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  firstname: string;
+
+  @Column()
+  lastname: string;
+
+  @Column()
+  adress: string;
+
+  @Column()
+  image: string;
+}
