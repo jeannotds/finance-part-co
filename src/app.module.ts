bdbb466fe2e1d05b048d24cbc4c06daf2daea9e4
jeannot diambu <diambujeannot@gmail.com>
2023-05-04 11:48:25 +0100

    change password connexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { ProfileModule } from './profile/profile.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'jeannot',
+      password: '',
       database: 'finance_part',
       entities: [User, Project],
       synchronize: true,
